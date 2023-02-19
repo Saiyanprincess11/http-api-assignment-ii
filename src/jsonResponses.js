9067b4b8715451efcf97161d044798906d459d68
Saiyanprincess11 <pth3035@rit.edu>
2023-02-19 14:24:32 -0500

    /getUsers and /notReal HEAD and GET methods done

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,9 +1,28 @@
+const users = {}; //Users Obj
+
+//JSON response methods 
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' }); 
+
+    //Header object 
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    response.writeHead(status, headers); 
     response.write(JSON.stringify(object)); 
     response.end(); 
 };
 
+const respondJSONMeta = (request, response, status) => {
+    const headers = {
+        'Content-Type': 'application/json', 
+    }
+
+    response.writeHead(status, headers); 
+    response.end(); 
+};
+
+//Methods 
 //404 Not Found 
 const notFound = (request, response) => {
     const responseJSON = {
@@ -14,24 +33,74 @@ const notFound = (request, response) => {
     respondJSON(request, response, 404, responseJSON); 
 };
 
-const getUsers = (request, response) => {
+const notFoundMeta = (request, response) => {
+    respondJSONMeta(request, response, 404); 
+}; 
 
+const getUsers = (request, response) => {
+    const responseJSON = {
+        users,
+    }; 
+
+    return respondJSON(request, response, 200, responseJSON); 
+}; 
+
+const getUsersMeta = (request, response) => {
+    return respondJSONMeta(request, response, 200); 
 }; 
 
 const addUser = (request, response) => {
+    const newUser = {
+        name: 'Bob',
+        age: 1, 
+    }; 
 
+    //Adds new user to Users list 
+    users.push(newUser); 
 
+    return respondJSON(request, response, 201, newUser); 
+}; 
+
+const updateUser = (request, response) => {
+
+    const editedUser = users; 
+
+    users[editedUser.age] = editedUser; 
+    //
+    return respondJSON(request, response, 204, editedUser); 
+
+};
+
+const missingParams = (request, response) => {
+
+};
+
+const addUserMeta = (request, response) => {
+    return respondJSONMeta(request, response, 201); 
 }; 
 
 const notReal = (request, response) => {
+    const responseJSON = {
+        message: 'The page your looking for is not real.',
+        id: 'notReal',
+    }; 
 
+    respondJSON(request, response, 404, responseJSON);
+}; 
+
+const notRealMeta = (request, response) => {
+    respondJSONMeta(request, response, 404);
 }; 
 
 //Exports 
-
 module.exports = {
     notFound, 
+    notFoundMeta, 
     getUsers, 
+    getUsersMeta,
     addUser, 
+    addUserMeta, 
+    updateUser, 
     notReal, 
+    notRealMeta
 };
